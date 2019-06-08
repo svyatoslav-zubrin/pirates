165c64093bd0e34d5297a777d34771c7b03ec111
Slava Zubrin <szubrin79@gmail.com>
2019-06-08 20:42:38 +0300

    Have single active tile (with the ship)

--- src/model/map.py
+++ src/model/map.py
@@ -38,7 +38,9 @@ class Map:
             New map object
         """
         self.size = rect.size
+
         self.grid_size = 22
+        self.activeTile = None
 
         self.tiles = [[] for _ in range(self.grid_size)]
         for i in range(0, self.grid_size):
@@ -49,6 +51,10 @@ class Map:
                 tile = Tile(tile_size, position)
                 self.__configureTile(tile)
                 self.tiles[i].append(tile)
+                if self.activeTile is None:
+                    if i == 1 & j == 1:
+                        tile.isActive = True
+                        self.activeTile = tile
 
     def render(self, screen):
         for tiles_line in self.tiles:
@@ -56,10 +62,12 @@ class Map:
                 tile.render(screen)
 
     def handle_click(self, position):
+        self.activeTile.isActive = False
         x = position[0] // (self.size[0] // self.grid_size)
         y = position[1] // (self.size[1] // self.grid_size)
         tile = self.tiles[x][y]
         tile.isActive = True
+        self.activeTile = tile
 
     def __configureTile(self, tile):
         tile_id = Initial_Board[tile.position[0]][tile.position[1]]
