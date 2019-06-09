ea2b9d06fa4753aeef54ac8831e47c63c4377e6b
Slava Zubrin <szubrin79@gmail.com>
2019-06-09 14:33:41 +0300

    Add dice

--- src/game_controller.py
+++ src/game_controller.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame import Rect
 from model.map import Map
+from model.dice import Dice
 
 
 class GameController:
@@ -8,12 +9,19 @@ class GameController:
         self.size = size
         self.title = title
         self.map_rect = Rect((0, 0), (self.size[1], self.size[1]))
+        self.dice_rect = Rect((self.size[1] + 50, 50), (200, 200))
+
+        self.all_sprites_group = pygame.sprite.LayeredDirty()
 
     def start(self):
         screen = pygame.display.set_mode(self.size)
         pygame.display.set_caption(self.title)
 
         map = Map(self.map_rect)
+        dice = Dice(self.dice_rect)
+
+        self.all_sprites_group.add(map.tiles_group.sprites())
+        self.all_sprites_group.add(dice)
 
         running = True
         while running:
@@ -24,9 +32,11 @@ class GameController:
                     position = pygame.mouse.get_pos()
                     if self.map_rect.collidepoint(position):
                         map.handle_click(position)
+                    elif self.dice_rect.collidepoint(position):
+                        dice.roll()
             # Draw everything
-            map.tiles_group.update()
-            rects = map.tiles_group.draw(screen)
+            self.all_sprites_group.update()
+            rects = self.all_sprites_group.draw(screen)
             pygame.display.update(rects)
 
 
---
+++ src/model/dice.py
@@ -0,0 +1,34 @@
+import os
+import random
+import pygame
+
+
+class Dice(pygame.sprite.DirtySprite):
+    def __init__(self, rect):
+        super(Dice, self).__init__()
+
+        self.rect = rect
+        self.value = 0
+        self.image = self.__image(rect.size)
+
+    def roll(self):
+        self.value = random.randint(1, 6)
+        self.image = self.__image(self.rect.size)
+        self.dirty = 1
+
+    def reset(self):
+        self.value = 0
+        self.dirty = 1
+
+    def __image(self, size):
+        main_dir = os.path.split(os.path.abspath(__file__))[0]
+        image_name = ["../../assets/dice/dice_0.png",
+                      "../../assets/dice/dice_1.png",
+                      "../../assets/dice/dice_2.png",
+                      "../../assets/dice/dice_3.png",
+                      "../../assets/dice/dice_4.png",
+                      "../../assets/dice/dice_5.png",
+                      "../../assets/dice/dice_6.png"]
+        path = os.path.join(main_dir, image_name[self.value])
+        image = pygame.image.load(path)
+        return pygame.transform.scale(image, size)
