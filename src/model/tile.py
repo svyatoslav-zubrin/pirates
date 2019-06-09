1ba3817513a8dd5fb0e5f58487a524d0207f7a56
Slava Zubrin <szubrin79@gmail.com>
2019-06-09 13:04:22 +0300

    Add board assets

--- src/model/tile.py
+++ src/model/tile.py
@@ -1,3 +1,4 @@
+import os
 import pygame
 from enum import Enum
 
@@ -29,8 +30,12 @@ class Tile(pygame.sprite.DirtySprite):
                                 self.position[1] * self.size[1],
                                 self.size[0],
                                 self.size[1])
-        self.image = pygame.Surface(self.rect.size)
-        self.image.fill(cfg.color())
+        bg_image = cfg.image(size)
+        if bg_image is None:
+            self.image = pygame.Surface(self.rect.size)
+            self.image.fill(cfg.color())
+        else:
+            self.image = bg_image
 
     def update(self):
         self.__draw()
@@ -46,9 +51,17 @@ class Tile(pygame.sprite.DirtySprite):
 class TileType(Enum):
     WATER = '.'
     GRASS = 'g'
-    WHARF = 'w'
     NEUTRAL_TRADE = 't'
     NEUTRAL_MILITARY = 'm'
+    # wharfs
+    WHARF_LEFT = '←'
+    WHARF_RIGHT = '→'
+    WHARF_UP = '↑'
+    WHARF_DOWN = '↓'
+    WHARF_TOP_LEFT = '↖︎'
+    WHARF_TOP_RIGHT = '↗︎'
+    WHARF_BOTTOM_LEFT = '↙︎'
+    WHARF_BOTTOM_RIGHT = '↘︎'
 
 
 class TileCfg:
@@ -58,14 +71,69 @@ class TileCfg:
     def color(self):
         if self.type == TileType.WATER:
             return (173, 216, 230)  # blue
-        elif self.type == TileType.WHARF:
-            return (139, 69, 19)  # brown
-        elif self.type == TileType.NEUTRAL_TRADE:
-            return (70, 130, 180)  # brown
-        elif self.type == TileType.NEUTRAL_MILITARY:
-            return (70, 130, 180)  # brown
-        else:  # grass
+        elif self.type == TileType.GRASS:
             return (34, 139, 34)  # green
+        elif self.type == TileType.NEUTRAL_TRADE:
+            return (70, 130, 180)  # dark blue
+        elif self.type == TileType.NEUTRAL_MILITARY:
+            return (70, 130, 180)  # dark blue
+        else:  # wharfs
+            return (139, 69, 19)  # brown
+
+    def image(self, size):
+        main_dir = os.path.split(os.path.abspath(__file__))[0]
+        if self.type == TileType.GRASS:
+            path = os.path.join(main_dir, '../../assets/grass1.png')
+            image = pygame.image.load(path)
+            return pygame.transform.scale(image, size)
+        elif self.type == TileType.WATER:
+            path = os.path.join(main_dir, '../../assets/water_light.png')
+            image = pygame.image.load(path)
+            return pygame.transform.scale(image, size)
+        elif self.type == TileType.NEUTRAL_TRADE or self.type == TileType.NEUTRAL_MILITARY:
+            path = os.path.join(main_dir, '../../assets/water_dark.png')
+            image = pygame.image.load(path)
+            return pygame.transform.scale(image, size)
+        elif self.type == TileType.WHARF_DOWN:
+            path = os.path.join(main_dir, '../../assets/pier_s.png')
+            image = pygame.image.load(path)
+            return pygame.transform.scale(image, size)
+        elif self.type == TileType.WHARF_UP:
+            path = os.path.join(main_dir, '../../assets/pier_s.png')
+            image = pygame.image.load(path)
+            image = pygame.transform.flip(image, False, True)
+            return pygame.transform.scale(image, size)
+        elif self.type == TileType.WHARF_LEFT:
+            path = os.path.join(main_dir, '../../assets/pier_s.png')
+            image = pygame.image.load(path)
+            image = pygame.transform.rotate(image, -90)
+            return pygame.transform.scale(image, size)
+        elif self.type == TileType.WHARF_RIGHT:
+            path = os.path.join(main_dir, '../../assets/pier_s.png')
+            image = pygame.image.load(path)
+            image = pygame.transform.rotate(image, 90)
+            return pygame.transform.scale(image, size)
+        elif self.type == TileType.WHARF_BOTTOM_RIGHT:
+            path = os.path.join(main_dir, '../../assets/pier_d.png')
+            image = pygame.image.load(path)
+            return pygame.transform.scale(image, size)
+        elif self.type == TileType.WHARF_BOTTOM_LEFT:
+            path = os.path.join(main_dir, '../../assets/pier_d.png')
+            image = pygame.image.load(path)
+            image = pygame.transform.rotate(image, -90)
+            return pygame.transform.scale(image, size)
+        elif self.type == TileType.WHARF_TOP_LEFT:
+            path = os.path.join(main_dir, '../../assets/pier_d.png')
+            image = pygame.image.load(path)
+            image = pygame.transform.flip(image, True, True)
+            return pygame.transform.scale(image, size)
+        elif self.type == TileType.WHARF_TOP_RIGHT:
+            path = os.path.join(main_dir, '../../assets/pier_d.png')
+            image = pygame.image.load(path)
+            image = pygame.transform.rotate(image, 90)
+            return pygame.transform.scale(image, size)
+        else:
+            return None
 
     def isSailable(self):
         if self.type == TileType.GRASS:
